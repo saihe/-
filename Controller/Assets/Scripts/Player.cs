1a9c59cba45bfd53766caab19a1cb4642c438a22
saihe <saihe.kyon@gmail.com>
2015-10-20 15:27:10 +0900

    フリック判定途中。

--- Controller/Assets/Scripts/Player.cs
+++ Controller/Assets/Scripts/Player.cs
@@ -6,6 +6,9 @@ public class Player : MonoBehaviour {
     //プレイヤーの移動スピード調整用変数
     public float speed = 1;
 
+    //フリック判定用
+    public float jump = 1;
+
     //タッチされた座標
     private Vector2 touch;
 
@@ -39,14 +42,30 @@ public class Player : MonoBehaviour {
             dragPoint = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
 
             //プレイヤーが移動するベクトル
-            float x = dragPoint.x - touch.x;
-            float y = 0;
-            float z = dragPoint.y - touch.y;
+            double x = dragPoint.x - touch.x;
+            double y = 0;
+            double z = dragPoint.y - touch.y;
 
+            double touchTime = Time.deltaTime;
+            if (touchTime < 0.05)
+            {
+                if (x < 0.05)
+                {
+                    print("Jump");
+                    transform.Translate(new Vector3(0, jump, 0));
+                }
+                else
+                {
+                    return;
+                }
+            }
+
+
+            //タッチ位置と移動位置が同じなら移動
             if (dragPoint != touch)
             {
                 //入力をVector3に変換移動量を制限
-                Vector3 direction = new Vector3(x, y, z) / 1000;
+                Vector3 direction = new Vector3((float)x, (float)y, (float)z) / 1000;
                 //print("direction: " + direction);
 
                 //入力ベクトルをQuaternionに変換
@@ -58,11 +77,10 @@ public class Player : MonoBehaviour {
                 //タッチされた座標を画面上の座標に変換
                 Vector3 cm = Camera.main.ScreenToWorldPoint(direction);
                 Vector3 moveTo = new Vector3(cm.x * -1, 0, cm.z * -1) / 100;
-                print("moveTo: " + moveTo);
+                print("direction: " + direction);
 
                 //移動
                 transform.Translate(moveTo * speed);
-                //print("rotation: " + transform.rotation.y);
             }
         }
     }
